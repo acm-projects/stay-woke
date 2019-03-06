960a468ec7a5bf6c3e1bb7856323d3263e35af73
Snipta Mallick <mallicksnip@gmail.com>
2019-03-06 15:26:25 -0600

    Updated colors and general layout

--- GC-Tab1.py
+++ GC-Tab1.py
@@ -3,12 +3,13 @@ import matplotlib.pyplot as plt
 # Pie chart, where the slices will be ordered and plotted counter-clockwise:
 labels = 'Yes', 'No', 'No opinion'
 sizes = [28 , 71, 1]
+colors = ['#ff9999','#66b3ff','#99ff99']
 
-fig1, ax1 = plt.subplots()
-ax1.pie(sizes, labels=labels, autopct='%1.1f%%',
-        shadow=False, startangle=90)
 
-# Equal aspect ratio ensures that pie is drawn as a circle.
-ax1.axis('equal') 
+fig, ax1 = plt.subplots()
 
+ax1.pie(sizes, labels=labels, colors=colors, autopct='%1.1f%%',
+        shadow=False, startangle = 90)
+
+ax1.axis('equal')  
 plt.show()
