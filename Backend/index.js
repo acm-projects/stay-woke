7e1ed4c772cb90bc3596de4b17ecf6c6fd850c0e
jonathan-harty <jonathanharty99@gmail.com>
2019-03-06 21:28:10 -0600

    added guardian api

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,20 @@
 const express = require('express')
+const request = require('request')
 const app = express()
 const fs = require('fs')
+const gKey = '14344d2d-2666-4857-96f8-18d26eed83bd'
+const gContent = 'https://content.guardianapis.com/search?'
+const gQuery = 'q=debate%20AND%20economy&tag=politics/politics&from-date=2014-01-01&api-key='
+
+const url = gContent + gQuery + gKey
 
 const log = console.log
 const port = process.env.PORT || 3000
 
 app.listen(port, () => log(`listening affectionitaly on ${port}`))
+
+request({ url: url, json: true }, (error, response) => {
+  const gData = response.body
+  log(gData)
+})
+
