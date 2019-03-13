64e52bbf5a7f8162c2db0ec7aa9c9a93228e30ef
jonathan-harty <jonathanharty99@gmail.com>
2019-03-13 16:48:22 -0500

    get urls from guardian api

--- Backend/index.js
+++ Backend/index.js
@@ -3,8 +3,8 @@ const request = require('request')
 const app = express()
 const fs = require('fs')
 const gKey = '14344d2d-2666-4857-96f8-18d26eed83bd'
-const gContent = 'https://content.guardianapis.com/search?'
-const gQuery = 'q=debate%20AND%20economy&tag=politics/politics&from-date=2014-01-01&api-key='
+const gContent = 'https://content.guardianapis.com/search'
+const gQuery = '?q=debate%20AND%20economy&tag=politics/politics&from-date=2014-01-01&api-key='
 
 const url = gContent + gQuery + gKey
 
@@ -13,8 +13,15 @@ const port = process.env.PORT || 3000
 
 app.listen(port, () => log(`listening affectionitaly on ${port}`))
 
-request({ url: url, json: true }, (error, response) => {
-  const gData = response.body
-  log(gData)
+request({ url : url, json : true }, (error, response) => {
+  const gData = response.body.response
+  const gResults = gData.results
+  let gUrls = []
+
+  gResults.forEach((article) => {
+    gUrls.push(article.webTitle)
+  })
+  
+  log(gUrls)
 })
 
