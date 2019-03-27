f6668b6912b26f883ca39f83b449058198fe3219
Swapnil Agrawal <swapnil@MacBook-Pro-2.local>
2019-03-27 18:58:19 -0500

    Add script.py

---
+++ Backend/script.py
@@ -0,0 +1,9 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+page = requests.get('https://web.archive.org/web/20121007172955/https://www.nga.gov/collection/anZ1.htm')
+
+soup = BeautifulSoup(page.text, 'html.parser')
+
+print (soup)
