5d3c2d25dfda6ff685a8f8a6b31e2ea103df6049
jonathan-harty <jonathanharty99@gmail.com>
2019-03-29 23:10:24 -0500

    Added ability to get current date in ISO format (YYYY/MM/DD)

--- Backend/guardian.js
+++ Backend/guardian.js
@@ -2,11 +2,21 @@ const request = require('request')
 
 const key = '14344d2d-2666-4857-96f8-18d26eed83bd'
 const content = 'https://content.guardianapis.com/search?'
+let tags = ''
+let currDate = new Date(Date.now())
+currDate = new Date(currDate.getTime() - (currDate.getTimezoneOffset() * 60000))
+currDate = currDate.toISOString().substring(0, 10)
 const query = 'q=debate%20AND%20economy&tag=politics/politics&from-date=2014-01-01&api-key='
 const log = console.log
 const url = content + query + key
 
-function getUrls() {
+log(currDate)
+
+function getUrls(apiUrl) {
+    if(true) {
+
+    }
+
 	request({ url : url, json : true }, (error, response) => {
     const data = response.body.response
     const results = data.results
@@ -16,8 +26,14 @@ function getUrls() {
       urls.push(result.webUrl)
     })
     
-    log(urls)
+    return urls
   })
+}
+
+function search(searchTerms) {
+    searchTerms.forEach((term) => {
+        tags += term
+    })
 }
 
 module.exports = {
