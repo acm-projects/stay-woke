53720f05dec8770f9dbc94dc037f737431a6c29b
Snipta Mallick <mallicksnip@gmail.com>
2019-04-03 20:48:01 -0500

    added text to data vis page

---
+++ homepage/src/components/pages/DataVis.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './../layout/Header';
+
+function DataVis() {
+  return (
+    <div>
+      <Header />
+      <h2>Data Visualization</h2>
+      <p>This is the Data Visualization page</p>
+    </div>
+  )
+}
+
+
+export default DataVis;
